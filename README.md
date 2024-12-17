177cc4b02b3ea4e61c4a0a17c77b16b7c42402ad
Rahul Rameshwar Rathod <rahulrathod741258@gmail.com>
2024-12-17 14:27:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FrontendAssignment
+ masaiclassAssignment
---
+++ todolist.js
@@ -0,0 +1,8 @@
+let toDoList=["Wake up", "Brush teeth","Have breakfast"]
+
+toDoList.push("Go for a walk")
+// console.log(toDoList)
+
+toDoList.pop()
+console.log(toDoList)
+
