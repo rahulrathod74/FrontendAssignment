a0cdad851eac3144ff710b06751a43e95de75f46
Rahul Rameshwar Rathod <rahulrathod741258@gmail.com>
2024-12-17 17:05:04 +0530

    object file

---
+++ Budget.js
@@ -0,0 +1,14 @@
+const bugget={
+    income:1000,
+    expenses:700,
+
+    // get saving(){
+    //     return this.income - this.expenses
+    // }
+}
+
+//  bugget.saving= bugget.income-bugget.expenses
+ bugget.income=1200
+ console.log(bugget.saving);
+ 
+
